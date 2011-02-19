5068b48d515c97494203beb7be5395c030cb5d13
Kore Nordmann <mail@kore-nordmann.de>
2011-02-19 15:31:07 +0100

    Implemented listing of unit abbreviations

--- army_factory.php
+++ army_factory.php
@@ -7,7 +7,7 @@ class ArmyFactory
      *
      * @var array
      */
-    protected static $mapping = array(
+    protected $mapping = array(
         // Own
         'R'   => 'Rekrut',
         'M'   => 'Miliz',
@@ -35,24 +35,37 @@ class ArmyFactory
     );
 
     /**
+     * Get abbreviation lsit
+     *
+     * Return a list with unit name abbreviations as index and class names as 
+     * value.
+     * 
+     * @return array
+     */
+    public function getAbbreviationList()
+    {
+        return $this->mapping;
+    }
+
+    /**
      * Build army from CLI specification string
      * 
      * @param string $specification 
      * @return Army
      */
-    public static function factory( $specification )
+    public function factory( $specification )
     {
         preg_match_all( '((?P<count>\\d+)(?P<type>[A-Z]+))', strtoupper( $specification ), $matches, PREG_SET_ORDER );
 
         $army = new Army();
         foreach ( $matches as $set )
         {
-            if ( !isset( self::$mapping[$set['type']] ) )
+            if ( !isset( $this->mapping[$set['type']] ) )
             {
                 throw new RuntimeException( "Unknown unit type {$set['type']}." );
             }
 
-            $class = self::$mapping[$set['type']];
+            $class = $this->mapping[$set['type']];
             $army->addUnitSet(
                 new UnitSet(
                     new $class(),
